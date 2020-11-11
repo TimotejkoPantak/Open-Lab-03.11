9a880e714144e24b152253709dfd4a5bea50cbc4
TimotejkoPantak <timopantakgit@gmail.com>
2020-11-11 13:11:49 +0100

    A function that checks, if the string is palindrome.

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_03._11
-{
+{ť
     public class Checker
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
+            if (str.SequenceEqual(str.Reverse()))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         } 
     }
 }
